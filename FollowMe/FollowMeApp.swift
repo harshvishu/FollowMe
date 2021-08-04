7ed84172c2d0d32c284662b78a7ae7e6e5c064d3
Harsh Vishwakarma <vishwakarma.harsh1993@gmail.com>
2021-08-04 16:21:22 +0530

    Initial Commit

---
+++ FollowMe/FollowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowMeApp.swift
+//  FollowMe
+//
+//  Created by harsh vishwakarma on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct FollowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
